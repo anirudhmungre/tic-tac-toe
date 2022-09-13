efbb5bbaa80986be41f2a020e35bfb7773e1acf0
Anirudh Mungre <anirudh.mungre16@gmail.com>
2022-09-13 12:19:36 -0400

    same size score board

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { RouterView } from "vue-router";
 
 <template>
   <main
-    class="grid flex-grow justify-center items-center text-center text-slate-400 bg-slate-900 h-screen"
+    class="grid justify-center items-center text-center text-slate-400 bg-slate-900 h-screen"
   >
     <RouterView />
   </main>
